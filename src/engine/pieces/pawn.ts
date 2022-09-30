270858a5692706370a12df5f29e4f0cf0337a47a
Bianca <bianca.darolti@softwire.com>
2022-09-30 12:24:32 +0100

    first pawn move 2 spaces added.

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,24 +4,36 @@ import Board from '../board';
 import Square from '../square'
 
 export default class Pawn extends Piece {
+
+    private blackStartingRow: number = 6;
+    private whiteStartingRow: number = 1;
+
     public constructor(player: Player) {
         super(player);
+
     }
 
     public getAvailableMoves(board: Board) {
 
         if (this.player === Player.BLACK) {
             const currSquare: Square = board.findPiece(this);
-            // if (!!board.getPiece(new Square(currSquare.row + 1, currSquare.col))) {
-            return new Array(new Square(currSquare.row - 1, currSquare.col));
-            // }
+            const availableMoves: Square[] = new Array();
+            if (currSquare.row === this.blackStartingRow) {
+                availableMoves.push(new Square(currSquare.row - 2, currSquare.col))
+            }
+            availableMoves.push(new Square(currSquare.row - 1, currSquare.col));
+            return availableMoves;
 
 
         } else {
             const currSquare: Square = board.findPiece(this);
-            // if (!!board.getPiece(new Square(currSquare.row - 1, currSquare.col))) {
-            return new Array(new Square(currSquare.row + 1, currSquare.col));
-            // }
+            const availableMoves: Square[] = new Array();
+            if (currSquare.row === this.whiteStartingRow) {
+                availableMoves.push(new Square(currSquare.row + 2, currSquare.col))
+            }
+            availableMoves.push(new Square(currSquare.row + 1, currSquare.col));
+            return availableMoves;
+            
         }
     }
 }
