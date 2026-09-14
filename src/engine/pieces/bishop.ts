da181e5bbfc586cdcd66b2d5a9ad09a23fe8351f
Pin-Softwire <Pinran.Jiang@softwire.com>
2026-09-14 11:55:40 +0100

    added bishop functionality

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,27 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = new Array();
+
+        const directions = [
+            { rowStep: -1, colStep: -1 },
+            { rowStep: -1, colStep: 1 },
+            { rowStep: 1, colStep: -1 },
+            { rowStep: 1, colStep: 1 }
+        ];
+
+        for (const direction of directions) {
+            let row = currSquare.row + direction.rowStep;
+            let col = currSquare.col + direction.colStep;
+
+            while (row >= 0 && row < GameSettings.BOARD_SIZE && col >= 0 && col < GameSettings.BOARD_SIZE) {
+                availableMoves.push(new Square(row, col));
+                row += direction.rowStep;
+                col += direction.colStep;
+            }
+        }
+
+        return availableMoves;
     }
 }
