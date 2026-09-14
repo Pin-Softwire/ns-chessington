8bf29919c70933e0de0cf5753a2fa2fd2f8006d2
Pin-Softwire <Pinran.Jiang@softwire.com>
2026-09-14 12:01:24 +0100

    done knight and queen movements

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,25 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = new Array();
+
+        const offsets = [
+            { rowStep: -2, colStep: -1 }, { rowStep: -2, colStep: 1 },
+            { rowStep: -1, colStep: -2 }, { rowStep: -1, colStep: 2 },
+            { rowStep: 1, colStep: -2 }, { rowStep: 1, colStep: 2 },
+            { rowStep: 2, colStep: -1 }, { rowStep: 2, colStep: 1 }
+        ];
+
+        for (const offset of offsets) {
+            const row = currSquare.row + offset.rowStep;
+            const col = currSquare.col + offset.colStep;
+
+            if (row >= 0 && row < GameSettings.BOARD_SIZE && col >= 0 && col < GameSettings.BOARD_SIZE) {
+                availableMoves.push(new Square(row, col));
+            }
+        }
+
+        return availableMoves;
     }
 }
--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,33 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = new Array();
+
+        const directions = [
+            // Horizontal and vertical
+            { rowStep: 0, colStep: -1 },
+            { rowStep: 0, colStep: 1 },
+            { rowStep: -1, colStep: 0 },
+            { rowStep: 1, colStep: 0 },
+            // Diagonal
+            { rowStep: -1, colStep: -1 },
+            { rowStep: -1, colStep: 1 },
+            { rowStep: 1, colStep: -1 },
+            { rowStep: 1, colStep: 1 }
+        ];
+
+        for (const direction of directions) {
+            let row = currSquare.row + direction.rowStep;
+            let col = currSquare.col + direction.colStep;
+
+            while (row >= 0 && row < GameSettings.BOARD_SIZE && col >= 0 && col < GameSettings.BOARD_SIZE) {
+                availableMoves.push(new Square(row, col));
+                row += direction.rowStep;
+                col += direction.colStep;
+            }
+        }
+
+        return availableMoves;
     }
 }
