80f8840bd3fe0eb1a642b33e698d43b7a6f6f985
Pin-Softwire <Pinran.Jiang@softwire.com>
2026-09-14 11:51:49 +0100

    implemented rook vertical/horizontal

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,23 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = new Array();
+
+        // Vertical
+        for (let row = 0; row < GameSettings.BOARD_SIZE; row++) {
+            if (row !== currSquare.row) {
+                availableMoves.push(new Square(row, currSquare.col));
+            }
+        }
+
+        // Horizontal
+        for (let col = 0; col < GameSettings.BOARD_SIZE; col++) {
+            if (col !== currSquare.col) {
+                availableMoves.push(new Square(currSquare.row, col));
+            }
+        }
+
+        return availableMoves;
     }
 }
